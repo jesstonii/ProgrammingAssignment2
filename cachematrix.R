542109924b573ea03e8cb1fe037c29cee139e941
jesstonii <77657903+jesstonii@users.noreply.github.com>
2021-01-19 16:01:34 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,27 @@
-<<<<<<< Updated upstream
-makeCacheMatrix <- function(x = matrix()){
+makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  set <- function(y){
+  set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() {x}
-  setInverse <- function(inverse) {inv <<- inverse}
-  getInverse <- function() {inv}
-  list(set = set, get = get, setInverse = setInverse, getInverse =getInverse)
-  
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-cacheSolve <- function(x, ...){
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
-  if(!is.null)(inv){
+  if (!is.null(inv)) {
     message("getting cached data")
     return(inv)
-    
   }
   mat <- x$get()
   inv <- solve(mat, ...)
   x$setInverse(inv)
   inv
 }
-
-=======
-makeCacheMatrix <- function(x = matrix()){
-  inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() {x}
-  setInverse <- function(inverse) {inv <<- inverse}
-  getInverse <- function() {inv}
-  list(set = set, get = get, setInverse = setInverse, getInverse =getInverse)
-  
-}
-
-cacheSolve <- function(x, ...){
-  inv <- x$getInverse()
-  if(!is.null)(inv){
-    message("getting cached data")
-    return(inv)
-    
-  }
-  mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setInverse(inv)
-  inv
-}
->>>>>>> Stashed changes
